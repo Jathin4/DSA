f233205625f7f95914b38bcd96e16df0962b59fb
GANGI JATHIN <jathingangi@gmail.com>
2026-08-10 06:53:44 +0530

    frequency_mapping_question(few)

--- frequency mapping & hashing.py
+++ frequency mapping & hashing.py
@@ -12,3 +12,27 @@ print(obj.freq_map(n))
 Time Complexity: O(n) — because the loop runs n times, and dictionary get() and insertion are O(1) average case.
 Space Complexity: O(k) — because the dictionary stores only the unique elements, where k = number of distinct elements; worst case O(n).
 """
+
+#Questions 
+class question1:
+    def ques1(self,a,b):
+        h = set(a)
+        for i in b:
+            if i not in h:
+                return False
+        return True
+obj = question1()
+a = [11, 1, 13, 21, 3, 7]
+b = [11, 3, 7, 1] 
+print(obj.ques1(a,b))
+
+
+class question2:
+    def ques2(self,str):
+        h = {}
+        for i in range(len(str)):
+            h[str[i]] = h.get(str[i],0)+1
+        return h
+obj = question2()
+str = "geeksforgeeks"
+print(obj.ques2(str))
